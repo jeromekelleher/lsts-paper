a56a122b451550fbaf7fedfc47f87d278fc87509
Jerome Kelleher <jk@well.ox.ac.uk>
2019-01-29 11:47:06 +0000

    Added outline for LS algorithms.

---
+++ src/algorithms.py
@@ -0,0 +1,20 @@
+"""
+Direct implementations of the algorithms in the paper.
+"""
+import numpy as np
+
+def ls_matrix(h, H):
+    """
+    Return the most likely path for the specified haplotype h through the
+    reference panel of haplotypes H.
+    """
+
+
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
