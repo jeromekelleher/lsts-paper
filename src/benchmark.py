34f51dbf57b1b6bd47c0387f0e28b36e99d0b6a7
Jerome Kelleher <jk@well.ox.ac.uk>
2019-03-26 11:11:54 +0000

    Fixed bug in ploidy output.

--- src/benchmark.py
+++ src/benchmark.py
@@ -10,15 +10,37 @@ import functools
 import gzip
 
 import msprime
+import tskit
 import numpy as np
 
 datadir = pathlib.Path("data")
 bulk_datadir = datadir / "bulk__NOBACKUP__"
 
+panel_sizes = np.logspace(3, 6, 20).astype(int) * 2
+
+def subset_sites(ts, position):
+    """
+    Return a copy of the specified tree sequence with sites reduced to those
+    with positions in the specified list.
+    """
+    tables = ts.dump_tables()
+    lookup = frozenset(position)
+    tables.sites.clear()
+    tables.mutations.clear()
+    for site in ts.sites():
+        if site.position in lookup:
+            site_id = tables.sites.add_row(
+                site.position, ancestral_state=site.ancestral_state,
+                metadata=site.metadata)
+            for mutation in site.mutations:
+                tables.mutations.add_row(
+                    site_id, node=mutation.node, parent=mutation.parent,
+                    derived_state=mutation.derived_state,
+                    metadata=mutation.metadata)
+    return tables.tree_sequence()
+
+
 def write_bcf(ts, filename):
-    # Had problems with this when run heavily in parallel, got
-    # Failed to open -: unknown file type
-    # from bcftools (presumably). No error code though.
     read_fd, write_fd = os.pipe()
     write_pipe = os.fdopen(write_fd, "w")
     with open(filename, "w") as bcf_file:
@@ -37,35 +59,41 @@ def run_simulation(panel_size, num_queries=1000, length=100):
         panel_size + num_queries, Ne=10**4, recombination_rate=1e-8,
         mutation_rate=1e-8, random_seed=42, length=length * 10**6)
     ts.dump(str(bulk_datadir / "{}_full.trees".format(panel_size)))
-    queries = np.arange(num_queries, dtype=np.int32)
     panel = np.arange(panel_size, dtype=np.int32) + num_queries
-    queries_ts = ts.simplify(queries, filter_sites=False)
-    queries_ts.dump(str(bulk_datadir / "{}_queries.trees".format(panel_size)))
-    panel_ts = ts.simplify(panel, filter_sites=False)
+    queries = np.arange(num_queries, dtype=np.int32)
+    panel_ts = ts.simplify(panel)
     panel_ts.dump(str(bulk_datadir / "{}_panel.trees".format(panel_size)))
+    queries_ts = ts.simplify(queries, filter_sites=False)
+    # Throw away any sites that are private to the query panel
+    queries_ts = subset_sites(queries_ts, panel_ts.tables.sites.position)
+    queries_ts.dump(str(bulk_datadir / "{}_queries.trees".format(panel_size)))
     if panel_size < 50000:
-        vcf_file = str(bulk_datadir / "{}_panel.vcf.gz".format(panel_size))
-        # with open(vcf_file, "w") as f:
-
-        with gzip.open(vcf_file, "wt") as f:
-            ts.write_vcf(f, ploidy=2)
-        # write_bcf(ts, str(bulk_datadir / "{}_panel.bcf".format(panel_size)))
+        # vcf_file = str(bulk_datadir / "{}_panel.vcf.gz".format(panel_size))
+        # with gzip.open(vcf_file, "wt") as f:
+        #     ts.write_vcf(f, ploidy=2)
+        write_bcf(ts, str(bulk_datadir / "{}_panel.bcf".format(panel_size)))
     return panel_size
 
 
 def run_simulations():
     num_queries = 1000
-    panel_size = np.logspace(3, 6, 20).astype(int)
 
     # run_simulation(panel_size[0], num_queries, length=1)
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = [executor.submit(run_simulation, n, num_queries) for n in panel_size]
-        for future in concurrent.futures.as_completed(future_to_url):
-            print("Completed", future.result)
+        futures = [executor.submit(run_simulation, n, num_queries, length=100) for n in panel_sizes]
+        for future in concurrent.futures.as_completed(futures):
+            print("Completed", future.result())
 
+def check_simulations():
+    for n in panel_sizes:
+        panel_ts = tskit.load(str(bulk_datadir / "{}_panel.trees".format(n)))
+        queries_ts = tskit.load(str(bulk_datadir / "{}_queries.trees".format(n)))
+        assert np.array_equal(panel_ts.tables.sites.position, queries_ts.tables.sites.position)
+        print(n, panel_ts.num_sites, queries_ts.num_sites)
 
 def main():
     run_simulations()
+    # check_simulations()
 
 if __name__ == "__main__":
     main()
